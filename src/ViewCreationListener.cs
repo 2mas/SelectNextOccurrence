e0227a3e2d163cc6f5820c6ca9f10d5fcd0f0686
Thomas Welén <thomas.welen@gmail.com>
2018-07-11 09:46:09 +0200

    Changing the Export-type of the viewcreationlistener to IVsTextViewCreationListener to get earlier access to commandchain.

--- src/ViewCreationListener.cs
+++ src/ViewCreationListener.cs
@@ -15,10 +15,10 @@ namespace SelectNextOccurrence
     /// Establishes an <see cref="IAdornmentLayer"/> to place the adornment on and exports the <see cref="IWpfTextViewCreationListener"/>
     /// that instantiates the adornment on the event of a <see cref="IWpfTextView"/>'s creation
     /// </summary>
-    [Export(typeof(IWpfTextViewCreationListener))]
+    [Export(typeof(IVsTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Editable)]
-    internal sealed class ViewCreationListener : IWpfTextViewCreationListener
+    internal sealed class ViewCreationListener : IVsTextViewCreationListener
     {
         // Disable "Field is never assigned to..." and "Field is never used" compiler's warnings. Justification: the field is used by MEF.
 #pragma warning disable 649, 169
@@ -56,9 +56,10 @@ namespace SelectNextOccurrence
         /// Instantiates a NextOccurrenceAdornment manager when the textView is created.
         /// </summary>
         /// <param name="textView">The <see cref="IWpfTextView"/> upon which the adornment should be placed</param>
-        public void TextViewCreated(IWpfTextView textView)
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
         {
 #pragma warning disable S1848 // Objects should not be created to be dropped immediately without being used
+            var textView = editorFactory.GetWpfTextView(textViewAdapter);
             new AdornmentLayer(
                     textView,
                     textSearchService,
